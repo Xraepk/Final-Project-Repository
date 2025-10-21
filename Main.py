18d681fb6e709b7ade7dd2c2957fc0b796bf7d8b
Xraepk <xraepk@gmail.com>
2025-10-20 22:36:26 -0600

    Started main menu

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ def main():
     import os
 
     user_file = ""
+    generate = False
 
     def clear():
         os.system('cls' if os.name == 'nt' else 'clear')
@@ -11,13 +12,16 @@ def main():
     def sign_in():
         global user_file
         logging_in = True
-        clear()
         while logging_in:
+            clear()
             has_account = input("Do you have an account? (Y/N) ").strip().upper()
             if has_account == "Y":
                 user_file = input("Enter account name: ")
                 if os.path.exists(user_file) and os.path.isfile(user_file):
                     print("Opening file...")
+                    logging_in = False
+                else:
+                    input("Invalid file. (Enter) ")
             elif has_account == "N":
                 user_file = input("Enter new file name: ")
                 if os.path.exists(user_file) or os.path.isfile(user_file):
@@ -28,12 +32,13 @@ def main():
                     clear()
             else:
                 clear()
-                print("Invalid input.", end="")
+                input("Invalid input. (Enter) ")
 
-
+    def main_menu():
+        while generate == False:
+            command = input("\tEnter \"A\" to begin.\nEnter \"B\" to create/edit presets.")
 
     sign_in()
-
         
 
 if __name__ == "__main__":
