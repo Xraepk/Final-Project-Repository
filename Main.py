ce33a32e65145fe8216495606d7749d759df5561
Xraepk <xraepk@gmail.com>
2025-10-21 14:13:29 -0600

    Started preset_manager

--- Main.py
+++ Main.py
@@ -4,10 +4,13 @@ def main():
     import os
 
     user_file = ""
-    generate = False
 
     def clear():
         os.system('cls' if os.name == 'nt' else 'clear')
+
+    def read_file(name):
+        with open(name) as file:
+            return file.readlines()
 
     def sign_in():
         global user_file
@@ -18,28 +21,46 @@ def main():
             if has_account == "Y":
                 user_file = input("Enter account name: ")
                 if os.path.exists(user_file) and os.path.isfile(user_file):
-                    print("Opening file...")
+                    input("File loaded. (Enter) ")
                     logging_in = False
                 else:
                     input("Invalid file. (Enter) ")
             elif has_account == "N":
                 user_file = input("Enter new file name: ")
                 if os.path.exists(user_file) or os.path.isfile(user_file):
-                    print("\nFile already exists. Enter new file name.\n")
+                    input("File already exists. Enter new file name. (Enter) ")
                 else:
                     open(user_file, "w").close()
                     input("File created. (Enter) ")
                     clear()
             else:
-                clear()
                 input("Invalid input. (Enter) ")
 
     def main_menu():
-        while generate == False:
-            command = input("\tEnter \"A\" to begin.\nEnter \"B\" to create/edit presets.")
+        clear()
+        begin = False
+        while begin == False:
+            command = input("Enter \"A\" to begin generation.\nEnter \"B\" to create/edit presets.\n\nEnter: ").strip()
+            if command.upper() == "A":
+                begin = True
+            elif command.upper() == "B":
+                preset_manager()
+
+    def preset_manager():
+        clear()
+        presets = read_file("user_file")
+        print("Current presets:")
+        for line in presets():
+            print(f"\t{line}")
+        print("\nEnter \"A\" to create a new preset.\nEnter \"B\" to edit existing preset.")
+
+
+    def generate():
+        return None
 
     sign_in()
-        
+    main_menu()
+    generate()
 
 if __name__ == "__main__":
     main()
